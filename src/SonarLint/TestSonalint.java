a8853f40531d41e21d57a2f08724dc049270c44e
SONDH6 <sondh6@pvcombank.com.vn>
2025-12-22 16:59:02 +0700

    commit

---
+++ src/SonarLint/TestSonalint.java
@@ -0,0 +1,11 @@
+package SonarLint;
+
+public class TestSonalint {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        Object a = new Object();
+
+        System.out.println(a);
+    }
+}
---
+++ src/Test/Test.java
@@ -0,0 +1,17 @@
+package Test;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Helo");
+        String[] a = new String[10];
+        String[] c = new String[10] ;
+        Arrays.asList("aa","ccc").toArray(c);
+
+        System.out.println(c.length);
+
+
+
+    }
+}
